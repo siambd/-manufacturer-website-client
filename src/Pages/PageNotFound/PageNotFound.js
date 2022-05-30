60bd1721b0351ecfebb28d29e26c28934ea815c6
siam <you@example.com>
2022-05-30 21:16:40 +0530

    not found added

---
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageNotFound.css'
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h2 className='text-4xl text-red-500 page-not-found text-center'>[404] Page not Found!!</h2>
+        </div>
+    );
+};
+
+export default PageNotFound;
